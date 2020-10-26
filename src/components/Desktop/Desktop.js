43b736446ffb235811ef6b87b27eedf801492777
germainruget <germain.ruget@gmail.com>
2020-10-26 16:32:17 +0100

    Fix height size

--- src/components/Desktop/Desktop.js
+++ src/components/Desktop/Desktop.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
+import useWindowSize from '../../hooks/windowSize';
 
 import classes from './Desktop.module.scss';
 
@@ -9,7 +10,9 @@ import NotificationBar from '../NotificationBar/NotificationBar';
 import Core from '../Core/Core';
 
 const Desktop = () => {
-
+   const windowSize = useWindowSize();
+   console.log(windowSize);
+   
    const [showMenu, setShowMenu] = useState(false);
    const [showSettings, setShowSettings] = useState(false);
    const [background, setBackground] = useState(defaultBG);
@@ -45,7 +48,7 @@ const Desktop = () => {
 
    if (loadBackground) {
       desktop = (
-         <div className={classes.Desktop} style={{ backgroundImage: `url(${background})` }}>
+         <div className={classes.Desktop} style={{ backgroundImage: `url(${background})`, height: windowSize.height }}>
             <NotificationBar />
             <Core showMenu={showMenu} showSettings={showSettings} displayMenu={displayMainMenu} setBg={changeBackground} />
             <AppBar displayMenu={displayMainMenu} showMenu={showMenu} displaySettings={displaySettings} showSettings={showSettings} />
